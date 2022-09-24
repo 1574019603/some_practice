efbb60cc3681f67f0a28543916b099ca56cafac9
wangxiao <1574019603@qq.com>
2022-09-24 19:09:11 +0800

    提交

--- Leetcode/src/MinimumDifferenceInStudentScores_1984.java
+++ Leetcode/src/MinimumDifferenceInStudentScores_1984.java
@@ -1,8 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MinimumDifferenceInStudentScores_1984 {
     public static void main(String[] args) {
 
     }
 
-
+    public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int result = Integer.MAX_VALUE;
+        for(int i = 0,j = k-1;j<n;i++,j++){
+            result = Integer.min(result,nums[j]-nums[i]);
+        }
+        return result;
+    }
 
 }
